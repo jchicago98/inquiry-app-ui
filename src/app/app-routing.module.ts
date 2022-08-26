f88f98241bfcbdd52ba8e61f7c28bdc831bf48f1
Morin <959926@cognizant.com>
2022-08-26 09:48:30 -0500

    Do not merge yet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { HomepageComponent } from './homepage/homepage.component';
 import { LoginComponent } from './login/login.component';
 import { RegistrationComponent } from './registration/registration.component';
 import { SearchpageComponent } from './searchpage/searchpage.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 export const routes: Routes = [
   {path:'homepage',component: HomepageComponent},
   {path:'login', component: LoginComponent},
   {path:'registration', component: RegistrationComponent},
-  {path:'searchpage', component: SearchpageComponent}
+  {path:'searchpage', component: SearchpageComponent},
+  {path:'sign-up', component:SignUpComponent}
 ];
 
 @NgModule({
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
-};
+  production: true,
+  cognito: {
+    userPoolId: 'us-west-2_9Lld0PCtn',
+    userPoolWebClientId: '2h45cs7sgvmbqufsqdpka9p4e9'
+},
+}
